dd9adafbc13bb40cdd5fd5e63803c18994695b7f
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2023-08-04 03:00:21 +0530

    edit vm

--- app/src/main/java/com/ashu/mftracker/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ashu/mftracker/ui/login/LoginViewModel.kt
@@ -1,8 +1,9 @@
 package com.ashu.mftracker.ui.login
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() {
+class LoginViewModel @Inject constructor(): ViewModel() {
 }
