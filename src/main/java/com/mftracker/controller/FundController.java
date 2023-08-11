acd642eef1a7441c3b29a21c3b24371ae24b2576
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2023-08-12 05:24:35 +0530

    added fund controller to search and fetch all mutual funds

---
+++ src/main/java/com/mftracker/controller/FundController.java
@@ -0,0 +1,50 @@
+package com.mftracker.controller;
+
+import com.mftracker.data.MutualFunds;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FundController {
+
+    @RequestMapping("/get-mutual-funds")
+    @ResponseBody
+    public List<MutualFunds> fetchMutualFundsList() {
+        String uri = "https://api.mfapi.in/mf";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MutualFunds[]> response =
+                restTemplate.getForEntity(
+                        uri,
+                        MutualFunds[].class);
+        MutualFunds[] funds = response.getBody();
+
+        System.out.println("result" + Arrays.stream(funds).toList());
+
+        return Arrays.stream(funds).toList();
+    }
+
+    @RequestMapping("/search-mutual-funds")
+    @ResponseBody
+    public List<MutualFunds> searchMutualFunds(@RequestParam(name = "keyWord")String keyWords) {
+        String uri = "https://api.mfapi.in/mf/search?q=" + keyWords;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MutualFunds[]> response =
+                restTemplate.getForEntity(
+                        uri,
+                        MutualFunds[].class);
+        MutualFunds[] funds = response.getBody();
+
+        System.out.println("result" + Arrays.stream(funds).toList());
+
+        return Arrays.stream(funds).toList();
+    }
+}
---
+++ src/main/java/com/mftracker/data/MutualFunds.java
@@ -0,0 +1,10 @@
+package com.mftracker.data;
+
+import lombok.Data;
+
+@Data
+public class MutualFunds {
+
+    private String schemeCode;
+    private String schemeName;
+}
