b54aaa55cada43e35d5093abbeb06ad615f863ce
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2023-08-29 06:19:37 +0530

    completed login flow

--- app/src/main/java/com/ashu/mftracker/global/utils/Constants.kt
+++ app/src/main/java/com/ashu/mftracker/global/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.ashu.mftracker.global.utils
 
 object Constants {
-    const val BASE_URL = "http://192.168.1.4:8084/"
-    const val BASE_URL_EMULATOR = "http://10.0.2.2:8083/"
+    const val BASE_URL = "http://192.168.1.3:8024/"
+    const val BASE_URL_EMULATOR = "http://10.0.2.2:8024/"
 }
